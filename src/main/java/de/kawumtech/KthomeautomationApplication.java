8010475bc09668766bffa5d2bc55fd0e5dfd6386
Peter H <pete@kawumtech.de>
2016-07-16 19:16:48 +0200

    switch to spring boot 1.4 and thymeleaf 3
    switched to spring boot 1.4, thymeleaf 3 and layout dialect 2.0;
    improved build script; removed no longer needed classes and
    configurations

--- src/main/java/de/kawumtech/KthomeautomationApplication.java
+++ src/main/java/de/kawumtech/KthomeautomationApplication.java
@@ -2,10 +2,8 @@ package de.kawumtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication(exclude={org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration.class})
-@EnableWebMvc
+@SpringBootApplication
 public class KthomeautomationApplication {
 
 	public static void main(String[] args) {
